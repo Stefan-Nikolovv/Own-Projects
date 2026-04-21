d0eab660f80f0b9f514abda23150cc8d7c68e333
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2026-04-21 12:13:08 +0300

    hot fix

---
+++ sample-JS-project/js/config.js
@@ -0,0 +1,8 @@
+// This file is gitignored - your secrets are safe!
+// For deployment: create this file with your production values
+
+export const config = {
+  supabaseUrl: "https://pghirikwblrhemnqcbmw.supabase.co",
+  supabaseAnonKey: "sb_publishable_yy_9eYxhcDQGxlIo4jgy3A_1Z165ZNp",
+  ownerEmail: "emotioninmotion@gmail.com",
+};
