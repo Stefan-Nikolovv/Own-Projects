c8830aa9cc9af400cecb61060e70c289fa038fc3
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2026-08-18 10:51:39 +0300

    fix: stabilize the admin roster layout

--- sample-JS-project/js/router.js
+++ sample-JS-project/js/router.js
@@ -1,6 +1,6 @@
 import { applyTranslations, t } from "./i18n.js";
 
-const APP_VERSION = "20260804-45";
+const APP_VERSION = "20260804-46";
 
 const routes = {
   "#home": {
--- sample-JS-project/service-worker.js
+++ sample-JS-project/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "emotion-in-motion-v45";
+const CACHE_NAME = "emotion-in-motion-v46";
 const APP_SHELL = [
   "/",
   "/index.html",
