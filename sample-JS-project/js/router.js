8323df69a27de723e83a9b74c3a9a531cbfa0081
Stefan-Nikolovv <stefan.nikolov4344@gmail.com>
2026-08-18 10:53:29 +0300

    fix: refine the roster button hover state

--- sample-JS-project/js/router.js
+++ sample-JS-project/js/router.js
@@ -1,6 +1,6 @@
 import { applyTranslations, t } from "./i18n.js";
 
-const APP_VERSION = "20260804-46";
+const APP_VERSION = "20260804-47";
 
 const routes = {
   "#home": {
--- sample-JS-project/service-worker.js
+++ sample-JS-project/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "emotion-in-motion-v46";
+const CACHE_NAME = "emotion-in-motion-v47";
 const APP_SHELL = [
   "/",
   "/index.html",
